bae67b9be00d7a0cc8f6ea51f9d2d30d988851a5
Jake Prem <jakeprem@gmail.com>
2019-09-27 16:33:09 -0400

    Add additional filters to Event View

--- lib/sauron_web/live/event_view.ex
+++ lib/sauron_web/live/event_view.ex
@@ -7,9 +7,23 @@ defmodule SauronWeb.EventView do
     ~L"""
     <div>
       <form phx-change="filter">
-        <input type="text" phx-change="filter" name="filter-text">
+        <input type="text" name="filter-text">
+        <table>
+          <tr>
+            <td>Hide streams?</td>
+            <td><input type="checkbox" name="hide-streams"></td>
+          </tr>
+          <tr>
+            <td>Show dataset events?</td>
+            <td><input type="checkbox" name="show-dataset" checked></td>
+          </tr>
+          <tr>
+            <td>Show  organization events?</td>
+            <td><input type="checkbox" name="show-org" checked></td>
+          </tr>
+        </table>
       </form>
-      <%= for val <- filter_values(@values, @filter) do %>
+      <%= for val <- filter_values(@values, @filter, @hide_streams, @show_dataset, @show_org) do %>
         <ul>
           <li>Event Type: <strong><%= val.type %></strong></li>
           <li>Author: <strong><%= val.author %></strong></li>
@@ -29,11 +43,26 @@ defmodule SauronWeb.EventView do
 
     events = EventAgent.get_events() |> Enum.map(&get_useful_info/1)
 
-    {:ok, assign(socket, values: events, filter: "")}
+    {:ok,
+     assign(socket,
+       values: events,
+       filter: "",
+       hide_streams: false,
+       show_dataset: true,
+       show_org: true
+     )}
   end
 
-  def handle_event("filter", %{"filter-text" => filter_text}, socket) do
-    {:noreply, assign(socket, filter: filter_text)}
+  def handle_event("filter", %{"filter-text" => filter_text} = changes, socket) do
+    IO.inspect(changes, label: "Changes")
+
+    {:noreply,
+     assign(socket,
+       filter: filter_text,
+       hide_streams: !!changes["hide-streams"],
+       show_dataset: !!changes["show-dataset"],
+       show_org: !!changes["show-org"]
+     )}
   end
 
   def handle_info(%{payload: event}, socket) do
@@ -49,7 +78,9 @@ defmodule SauronWeb.EventView do
       dataset_id: event.data.id,
       org_title: event.data.business.orgTitle,
       data_title: event.data.business.dataTitle,
-      time: event.create_ts
+      time: event.create_ts,
+      is_stream: SmartCity.Dataset.is_stream?(event.data),
+      struct_type: event.data.__struct__
     }
   end
 
@@ -60,7 +91,9 @@ defmodule SauronWeb.EventView do
       dataset_id: event.data.id,
       org_title: event.data.orgTitle,
       data_title: "n/a",
-      time: event.create_ts
+      time: event.create_ts,
+      is_stream: false,
+      struct_type: event.data.__struct__
     }
   end
 
@@ -71,13 +104,18 @@ defmodule SauronWeb.EventView do
       dataset_id: "???",
       org_title: "???",
       data_title: "???",
-      time: event.create_ts
+      time: event.create_ts,
+      is_stream: false,
+      struct_type: :unknown
     }
   end
 
-  defp filter_values(values, filter) do
+  defp filter_values(values, filter, hide_streams, show_dataset, show_org) do
     values
     |> Enum.filter(&String.contains?(inspect(&1), filter))
+    |> Enum.reject(fn val -> hide_streams && val.is_stream end)
+    |> Enum.reject(fn val -> !show_dataset && val.struct_type == SmartCity.Dataset end)
+    |> Enum.reject(fn val -> !show_org && val.struct_type == SmartCity.Organization end)
     |> Enum.take(5000)
   end
 end
